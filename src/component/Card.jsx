75434405caf18a32c1b8588de27ecdd881405b46
Hmmam <bhmmam@gmail.com>
2025-07-04 19:54:12 +0300

    refactor: simplify card rendering logic by using icon mapping and removing unused variables

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -4,11 +4,20 @@ import WildIcon from "../SVG/download.svg?react"
 
 export default function Card({color, value}) {
     const isWild = color === "wild" 
-    let displayValue = value;
-    const isSkip = value === "skip";
-    const isReverse = value === "reverse"
-    const isChangeColor = value === "wild"
-    const isNumberCard = !isReverse && !isSkip && !isChangeColor;
+    // let displayValue = value;
+    // const isSkip = value === "skip";
+    // const isReverse = value === "reverse"
+    // const isChangeColor = value === "wild"
+    // const isNumberCard = !isReverse && !isSkip && !isChangeColor;
+
+    const icons = {
+        skip : SkipIcon,
+        reverse : ReverseIcon,
+        wild : WildIcon,
+    };
+
+    const Icon = icons[value]
+
 
     const WildCard =()=>(
         <div className="table-squares">
@@ -22,10 +31,13 @@ export default function Card({color, value}) {
     return (
         <div className={`card card-${color}`}>
             <div className="card-content">
-                {isReverse && <div className="card-value"><ReverseIcon className="card-icon"/></div>}
-                {isSkip && <div className="card-value"><SkipIcon className="card-icon"/></div>}
-                {isChangeColor && <div className="card-value"><WildIcon className="card-icon"/></div>}
-                {isNumberCard && (<span className="card-value">{displayValue}</span>)}
+                {Icon ?
+                    (<div className="card-value">
+                        <Icon className="card-icon"/>
+                    </div>)
+                    : 
+                    (<span className="card-value">{value}</span>)
+                }
                 {isWild && <WildCard />}
             </div>
         </div>
